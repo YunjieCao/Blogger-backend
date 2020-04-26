99f660090cab99f1dc2eaf73f6b6b7ec5650810e
yunjie <YunjieCao@hotmail.com>
2020-04-26 11:35:31 -0400

    delete useless code in user_info.py

--- blogger_backend/Users/user_info.py
+++ blogger_backend/Users/user_info.py
@@ -18,27 +18,3 @@ def get_profile(request, user_id):
 
     return ret
 
-def try_connect():
-    db_connect_info = {
-        'host': 'blogger-mysql.cp2o8exigrko.us-east-1.rds.amazonaws.com',
-        'user': 'admin',
-        'password': 'cloudcomputing',
-        'port': 3306,
-        'db': 'blogger',
-        'charset': 'utf8'
-    }
-    c_info = db_connect_info
-    result = pymysql.connect(
-        host=c_info['host'],
-        user=c_info['user'],
-        password=c_info['password'],
-        port=c_info['port'],
-        db=c_info['db'],
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    print('successfully connect')
-
-
-if __name__=="__main__":
-    try_connect()
