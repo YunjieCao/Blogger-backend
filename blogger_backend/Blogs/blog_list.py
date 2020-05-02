161e65d2f7fed43680ec125b24d873bc9831b181
yunjie <YunjieCao@hotmail.com>
2020-05-02 08:30:22 -0400

    adapt backend code for user profile requests

--- blogger_backend/Blogs/blog_list.py
+++ blogger_backend/Blogs/blog_list.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 import json
 from BloggerModel.models import Blogs
-
+from BloggerModel.models import Users
 
 def get_user_blog_list(request, user_id):
     blog_list = Blogs.objects.filter(author_id=user_id).values('title', 'id', 'timestamp', 'description').order_by('-timestamp')[:10]
+    user_info = Users.objects.filter(id=user_id).values('name', 'avatar')  # list of objects
+    user_info = user_info[0]
+    author = user_info['name']
+    avatar = user_info['avatar']
+
     ret_data = {}
     ret_list = []
     for i in range(len(blog_list)):
         blog_list[i]['timestamp'] = json.dumps(blog_list[i]['timestamp'], indent=4, sort_keys=True, default=str)
+        blog_list[i]['author'] = author
+        blog_list[i]['avatar'] = avatar
         ret_list.append(blog_list[i])
     ret_data['blogs'] = ret_list
 
--- blogger_backend/Users/user_info.py
+++ blogger_backend/Users/user_info.py
@@ -14,10 +14,10 @@ def get_profile(request, user_id):
     # get all attributes from database
     # user_info = Users.objects.filter(id=user_id).values()
     # get specific attributes from database
-    user_info = Users.objects.filter(id=user_id).values('name', 'birthday', 'occupation', 'introduction') # list of objects
+    user_info = Users.objects.filter(id=user_id).values('name', 'birthday', 'occupation', 'introduction', 'timestamp', 'avatar') # list of objects
     user_info = user_info[0]
     user_info['birthday'] = json.dumps(user_info['birthday'], indent=4, sort_keys=True, default=str) # jsonify datatime
-
+    user_info['timestamp'] = json.dumps(user_info['timestamp'], indent=4, sort_keys=True, default=str)
     ret = HttpResponse(json.dumps(user_info))
     ret['Access-Control-Allow-Origin'] = '*'
     # ret['Content-Type'] = 'text/html'
